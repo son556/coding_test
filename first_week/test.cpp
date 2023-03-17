c9b49856075c69284a8667d2f7cd88f36ea5675f
son556 <shhmun02@naver.com>
2023-03-17 16:48:21 +0900

    이론

--- first_week/test.cpp
+++ first_week/test.cpp
@@ -2,12 +2,22 @@
 
 //제출 시  #include <bits/stdc++.h>
 using namespace std;
-#define PI 3.141592
-#define loop(x,n) for (int x = 0; x < n; x++)
-int main() {
-	string s;
-	getline(cin, s);
-	cout << s << '\n';
+int a[10];
+int b[10][10];
+
+int main()
+{
+	fill(a,a + 10, 100);
+	for(int i = 0; i < 10; i++){ 
+		cout << a[i] << " ";
+	}
+	cout << '\n';
+	fill(&b[0][0], &b[0][0] + 10 * 10, 2); 
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){ 
+			cout << b[i][j] << " ";
+		}
+		cout << '\n'; 
+	}
 	return 0;
 }
-
