07d7f5247c0d052bcc45e36db2dfd0925bf9dea4
son556 <shhmun02@naver.com>
2023-04-05 22:30:59 +0900

    5

--- test/test.c
+++ test/test.c
@@ -6,133 +6,60 @@
 /*   By: chanson <chanson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:51:20 by chanson           #+#    #+#             */
-/*   Updated: 2023/03/07 19:43:32 by chanson          ###   ########.fr       */
+/*   Updated: 2023/03/30 21:16:14 by chanson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <termios.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <termcap.h>
-#include <signal.h>
 
-# define BACKSPACE 127
-# define LEFT_ARROW 4479771
-# define RIGHT_ARROW 4414235
-
-int	nbr_length(int n)
+char	*ft_strcpy(char *str)
 {
-	int	i = 0;
+	char	*temp;
+	int		i;
 
-	if (n <= 0)
+	i = 0;
+	while (str[i])
 		i++;
-	while (n != 0)
+	temp = (char *)malloc(sizeof(char) * (i + 1));
+	temp[i] = '\0';
+	i = -1;
+	while (str[++i])
+		temp[i] = str[i];
+	return (temp);
+}
+
+int main(int	argc, char **argv, char **envp)
+{
+    int pid, child_pid, status;
+	char	**cmd;
+	cmd = (char **)malloc(sizeof(char *) * 2);
+	cmd[0] = ft_strcpy("/bin/lt");
+	cmd[1] = NULL;
+    (void)argc;
+	(void)argv;
+	if ((pid=fork()) == -1)
+       perror("fork failed");
+    /* 부모 프로세스 */
+    else if (pid != 0) 
 	{
-		n /= 10;
-		i++;
-	}
-	return (i);
-}
-
-void	get_cursor_position(int *col, int *rows)
-{
-	int		a = 0;
-	int		i = 1;
-	char	buf[255];
-	int		ret;
-	int		temp;
-
-	write(0, "\033[6n", 4);  //report cursor location
-	ret = read(0, buf, 254);
-	buf[ret] = '\0';
-	while (buf[i])
-	{
-		if (buf[i] >= '0' && buf[i] <= '9')
-		{
-			if (a == 0)
-				*rows = atoi(&buf[i]) - 1;
-			else
-			{
-				temp = atoi(&buf[i]);
-				*col = temp - 1;
-			}
-			a++;
-			i += nbr_length(temp) - 1;
-		}
-		i++;
-	}
-}
-
-int		putchar_tc(int tc)
-{
-	write(1, &tc, 1);
-	return (0);
-}
-
-void	move_cursor_left(int *col, int *row, char *cm)
-{
-	if (*col == 0)
-		return ;
-	--(*col);
-	tputs(tgoto(cm, *col, *row), 1, putchar_tc);
-
-}
-
-void	move_cursor_right(int *col, int *row, char *cm)
-{
-	char	*cam;
-
-	cam = tgetstr("cm", NULL);
-	++(*col);
-	tputs(tgoto(cam, *col, *row), 1, putchar_tc);
-
-}
-
-void	delete_end(int *col, int *row, char *cm, char *ce)
-{
-	if (*col != 0)
-		--(*col);
-	tputs(tgoto(cm, *col, *row), 1, putchar_tc);
-	tputs(ce, 1, putchar_tc);
-}
-
-int		main(void)
-{
-	/* change term settings */
-	struct termios term;
-	tcgetattr(STDIN_FILENO, &term);
-	term.c_lflag &= ~ICANON;
-	term.c_lflag &= ~ECHO;
-	term.c_cc[VMIN] = 1;
-	term.c_cc[VTIME] = 0;
-	tcsetattr(STDIN_FILENO, TCSANOW, &term);
-
-	/* init termcap */
-	tgetent(NULL, "xterm");
-	char *cm = tgetstr("cm", NULL); //cursor motion
-	char *ce = tgetstr("ce", NULL); //clear line from cursor
-	
-	int c = 0;
-	int row;
-	int col;
-
-	signal(SIGINT, SIG_IGN);
-	while (read(0, &c, sizeof(c)) > 0)
-	{
-		printf("%c\n", c);
-		get_cursor_position(&col, &row);
-		if (c == LEFT_ARROW)
-			move_cursor_left(&col, &row, cm);
-		else if (c == RIGHT_ARROW)
-			move_cursor_right(&col, &row, cm);
-		else if (c == BACKSPACE)
-			delete_end(&col, &row, cm, ce);
-		else
-		{
-			col++;
-			write(0, &c, 1);
-		}
-		c = 0;
-	}
+       pid = waitpid(pid, &status, 0);
+	   printf("status: %d\n", status);
+       if (WIFEXITED(status))
+	   {
+          printf("child terminated with code %d\n", WEXITSTATUS(status));
+		  status = WEXITSTATUS(status);
+		  printf("st: %d\n", status);
+	   }
+       else
+          printf("child terminated abnormally\n");
+    /* 자식 프로세스 */
+    } else {
+		int n
+       printf("run child\n");
+	   exit(execve(cmd[0], cmd, envp));   /* 자식 프로세스는 종료 코드 27을 반환하고 종료 */
+    }
 }
